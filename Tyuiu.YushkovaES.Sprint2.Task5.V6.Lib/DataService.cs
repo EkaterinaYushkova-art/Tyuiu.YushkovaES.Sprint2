5b309abf5a44e5511ab1ed4adde7ef8335e87a95
Юшкова Екатерина <yekaterina.yushkova.07@mail.ru>
2025-10-11 16:44:56 +0300

    Task6

--- Tyuiu.YushkovaES.Sprint2.Task5.V6.Lib/DataService.cs
+++ Tyuiu.YushkovaES.Sprint2.Task5.V6.Lib/DataService.cs
@@ -4,66 +4,56 @@ namespace Tyuiu.YushkovaES.Sprint2.Task5.V6.Lib
 {
     public class DataService : ISprint2Task5V6
     {
+        public string FindCardNameAndSuit(int value, int suit)
+        {
+            if (suit < 1 || suit > 4)
+            {
+                throw new ArgumentException("Номер масти должен быть от 1 до 4");
+            }
+
+            if (value < 6 || value > 14)
+            {
+                throw new ArgumentException("Номер достоинства должен быть от 6 до 14");
+            }
+
+            string valueName = GetValueName(value);
+            string suitName = GetSuitName(suit);
+
+            return $"{valueName} {suitName}";
+        }
+
+        private string GetValueName(int value)
+        {
+            switch (value)
+            {
+                case 6: return "Шестерка";
+                case 7: return "Семерка";
+                case 8: return "Восьмерка";
+                case 9: return "Девятка";
+                case 10: return "Десятка";
+                case 11: return "Валет";
+                case 12: return "Дама";
+                case 13: return "Король";
+                case 14: return "Туз";
+                default: throw new ArgumentException("Неизвестное достоинство карты");
+            }
+        }
+
+        private string GetSuitName(int suit)
+        {
+            switch (suit)
+            {
+                case 1: return "пик";
+                case 2: return "треф";
+                case 3: return "бубен";
+                case 4: return "черв";
+                default: throw new ArgumentException("Неизвестная масть");
+            }
+        }
+
         public string FindCardNameAndValue(int value1, int value2)
         {
-            string suit;
-            string rank;
-
-            // Определяем масть
-            switch (value1)
-            {
-                case 1:
-                    suit = "пик";
-                    break;
-                case 2:
-                    suit = "треф";
-                    break;
-                case 3:
-                    suit = "бубен";
-                    break;
-                case 4:
-                    suit = "червей";
-                    break;
-                default:
-                    throw new ArgumentException($"Номер масти должен быть от 1 до 4. Значение {value1}");
-            }
-
-            // Определяем достоинство
-            switch (value2)
-            {
-                case 6:
-                    rank = "Шестёрка";
-                    break;
-                case 7:
-                    rank = "Семёрка";
-                    break;
-                case 8:
-                    rank = "Восьмёрка";
-                    break;
-                case 9:
-                    rank = "Девятка";
-                    break;
-                case 10:
-                    rank = "Десятка";
-                    break;
-                case 11:
-                    rank = "Валет";
-                    break;
-                case 12:
-                    rank = "Дама";
-                    break;
-                case 13:
-                    rank = "Король";
-                    break;
-                case 14:
-                    rank = "Туз";
-                    break;
-                default:
-                    throw new ArgumentException($"Номер достоинства карты должен быть от 6 до 14. Значение {value2}");
-            }
-
-            // Формируем полное название карты
-            return $"{rank} {suit}";
+            throw new NotImplementedException();
         }
     }
 }
--- Tyuiu.YushkovaES.Sprint2.Task6.V3.Lib/DataService.cs
+++ Tyuiu.YushkovaES.Sprint2.Task6.V3.Lib/DataService.cs
@@ -9,31 +9,24 @@ namespace Tyuiu.YushkovaES.Sprint2.Task6.V3.Lib
             string res;
             switch (value)
             {
-                case 1:
-                    res = "понедельник";
-                    break;
-                case 2:
-                    res = "вторник";
-                    break;
-                case 3:
-                    res = "среда";
-                    break;
-                case 4:
-                    res = "четверг";
-                    break;
-                case 5:
-                    res = "пятница";
-                    break;
-                case 6:
-                    res = "суббота";
-                    break;
-                case 7:
-                    res = "воскресенье";
-                    break;
-                default:
-                    throw new ArgumentException($"Неделя должна быть от 1 до 7. Значение {value}");
+                case 1: return "понедельник";
+                   
+                case 2: return "вторник";
+                    
+                case 3: return "среда";
+                    
+                case 4: return "четверг";
+                    
+                case 5: return "пятница";
+                    
+                case 6: return "суббота";
+                    
+                case 7: return "воскресенье";
+                   
+                default: throw new ArgumentException($"Неделя должна быть от 1 до 7. Значение {value}");
+
             }
-            return res;
+            
         }
     }
 }
