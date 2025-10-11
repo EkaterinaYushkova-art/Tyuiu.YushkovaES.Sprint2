69f2d364c970ac4b867ce464c1753ab62196518a
Юшкова Екатерина <yekaterina.yushkova.07@mail.ru>
2025-10-11 20:56:01 +0300

    Task7

---
+++ Tyuiu.YushkovaES.Sprint2.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.YushkovaES.Sprint2.Task7.V1.Lib
+{
+    public class DataService : ISprint2Task7V1
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if ((y >= -x - 1) && (y <= -x + 1) && (y >= x - 1) && (y <= x + 1))
+
+
+
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+
+    }
+}
---
+++ Tyuiu.YushkovaES.Sprint2.Task7.V1/Program.cs
@@ -0,0 +1,55 @@
+﻿using Tyuiu.YushkovaES.Sprint2.Task7.V1.Lib;
+namespace Tyuiu.YushkovaES.Sprint2.Task7.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнила: Екатерина Е.С | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                *");
+            Console.WriteLine("* Задание #7                                                             *");
+            Console.WriteLine("* Вариант #1                                                             *");
+            Console.WriteLine("* Выполнила: Екатерина Е.С | ИСПб-25-1                                   *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные данные          *");
+            Console.WriteLine("* (вещественные значения) и вычисляет, находится ли точка                *");
+            Console.WriteLine("*с координатами X,Y в заштрихованной области.                            *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.Write("Введите координату X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите координату Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            if (res)
+            {
+                Console.WriteLine($"* Точка с координатами ({x}, {y}) находится в заштрихованной области *");
+            }
+            else
+            {
+                Console.WriteLine($"* Точка с координатами ({x}, {y}) НЕ находится в заштрихованной области *");
+            }
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
